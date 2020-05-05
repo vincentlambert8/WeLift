12f1ff492108c20b49dc023af414a75137c85181
vincentlambert8 <vincent.lambert.8@ulaval.ca>
2020-05-05 18:25:10 -0400

    Search

--- src/application/client/searchlift.py
+++ src/application/client/searchlift.py
@@ -7,60 +7,64 @@ from domain.server.connection_db import get_db
 templates_dir = os.path.abspath('presentation/templates')
 search = Blueprint('search', __name__, template_folder=templates_dir)
 
-@search.route('/searchlift', methods=(['POST']))
+@search.route('/searchlift', methods=['GET', 'POST'])
 def searchlift():
-    print("searCH")
+    if(session.get('ID', None) is not None):
+        conn = get_db()
+        cur = conn.cursor()
 
-    conn = get_db()
-    cur = conn.cursor()
+        if(request.method == 'POST'):
+            if request.form['departure'] != "":
+                departure = request.form['departure']
+                departureQuery = " T.departure LIKE '%{}%' AND".format(departure)
+            else:
+                departureQuery = ""
 
-    if request.form['departure'] != "":
-        departure = request.form['departure']
-        departureQuery = " T.departure LIKE '%{}%' AND".format(departure)
+
+            if request.form['destination'] != "":
+                destination = request.form['destination']
+                destinationQuery = " T.destination LIKE '%{}%' AND".format(destination)
+            else:
+                destinationQuery = ""
+
+
+            if request.form['date'] != "":
+                date = request.form['date']
+                dateQuery = " T.date LIKE %'{}'% AND".format(date)
+            else:
+                dateQuery = ""
+
+
+            if request.form['seats'] != "":
+                numberOfPassengers = request.form['seats']
+                numberOfPassengersQuery = " T.seats_available >= '{}' AND".format(numberOfPassengers)
+            else:
+                numberOfPassengers = 1
+                numberOfPassengersQuery = " T.seats_available >= 1 AND"
+
+
+            if request.form['maxPrice'] != "":
+                maxPrice = request.form['maxPrice']
+                maxPriceQuery = " T.Price <= '{}'".format(maxPrice)
+            else:
+                maxPriceQuery = " T.Price > 0"
+
+
+            command = "SELECT * FROM trips T WHERE" + departureQuery + destinationQuery + dateQuery + numberOfPassengersQuery + maxPriceQuery + " AND date > NOW() ORDER BY date"
+            cur.execute(command)
+            trajets = cur.fetchall()
+
+        else:
+            command = "SELECT * FROM trips WHERE seats_available != 0 AND date > NOW() ORDER BY date"
+            cur.execute(command)
+            trajets = cur.fetchall()
+
+        cur.close()
+        conn.close()
+        return render_template("searchlift.html",trajets=trajets)
+
     else:
-        departureQuery = ""
-
-
-    if request.form['destination'] != "":
-        destination = request.form['destination']
-        destinationQuery = " T.destination LIKE '%{}%' AND".format(destination)
-    else:
-        destinationQuery = ""
-
-
-    if request.form['date'] != "":
-        date = request.form['date']
-        dateQuery = " T.date LIKE %'{}'% AND".format(date)
-    else:
-        dateQuery = ""
-
-
-    if request.form['numberOfPassengers'] != "":
-        numberOfPassengers = request.form['numberOfPassengers']
-        numberOfPassengersQuery = " T.seats_available >= '{}' AND".format(numberOfPassengers)
-    else: 
-        numberOfPassengers = 1
-        numberOfPassengersQuery = " T.seats_available >= 1 AND"
-
-
-    if request.form['maxPrice'] != "":
-        maxPrice = request.form['maxPrice']
-        maxPriceQuery = " T.Price <= '{}'".format(maxPrice)
-    else: 
-        maxPriceQuery = " T.Price > 0"
-
-    
-    command = "SELECT * FROM trips T WHERE" + departureQuery + destinationQuery + dateQuery + numberOfPassengersQuery + maxPriceQuery
-    cur.execute(command)
-    print(command)
-
-    trajets = cur.fetchall()
- 
-    cur.close()
-    conn.close()
-    return render_template("searchlift.html",trajets=trajets)
-
-
+        return redirect('../home')
 
 
  
