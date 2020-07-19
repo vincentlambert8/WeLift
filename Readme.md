8ad3d5d2bd813a7c0c868e2a333d1b9e1b3da00c
vincentlambert8 <vincent.lambert.8@ulaval.ca>
2020-07-19 17:08:27 -0400

    Update readme

--- Readme.md
+++ Readme.md
@@ -1,11 +1,42 @@
-# Architecture
+# WeLift
 
-## Domaine
+As part of this project, we decided to create a carpooling application that will be used by travelers across Quebec to get around in a simple and ecological way.
 
-### MySQL commands list here: 
+### Requirements
 
-http://g2pc1.bu.edu/~qzpeng/manual/MySQL%20Commands.htm
+##### 1. MySQL
 
-## Application
+- The project relies on **MySQL Server** as our database. Go ahead, and install it first from [this page](https://dev.mysql.com/downloads/installer/).
 
-## Presentation
+- Add a MySQL Server instance in the installer. Follow the instructions.
+
+- Once the database is set up, open the MySQL Commend Line Client. It can be found from the system search bar. You will be asked to create an account.
+
+- Copy paste the sql code from [here](src/domain/server/welift_db.sql).
+
+- Create a file in [this directory](src/domain/server) called ```db.yaml``` and insert these information. The **mysql_password** is the one you configure previously.
+
+  ```
+  mysql_host: 'localhost'
+  mysql_user: 'root'
+  mysql_password: 'mypassword'
+  mysql_db: 'welift'
+  ```
+
+##### 2. Install the project's libraries with the following command:
+
+```
+$ pip install -r requirements.txt
+```
+
+### Usage
+
+1. Make sure you are using the virtual environment.
+
+2. Run the program by opening the command line in the project's ```src``` directory and enter the following command:
+
+   ```
+   $ py app.py
+   ```
+
+3. Go to [this site](http://localhost:5000/home) and enjoy!
--- src/domain/server/welift_db.sql
+++ src/domain/server/welift_db.sql
@@ -30,37 +30,21 @@ CREATE TABLE Transactions(id int AUTO_INCREMENT, id_transmitter int, id_recipien
 CREATE TABLE Comments(id int AUTO_INCREMENT, message varchar(300), score int, id_passenger int, id_driver int, primary key (id), foreign key (id_passenger) references Users(id), foreign key (id_driver) references Trips(id_driver));
 CREATE INDEX idIndex USING HASH ON Comments(id_driver);
 
-
+/* User table INSERT example */
 INSERT INTO Users VALUES (NULL, 'lamby@ulaval.ca', MD5('lamby'), 'Vincent', 'Lambert', 'Male', '1998-01-01', 'Québec', '(418) 507-0000', 150);
-INSERT INTO Users VALUES (NULL, 'yuk@ulaval.ca', MD5('yuk'), 'Yuxuan', 'Zhao', 'Male', '1998-01-01', 'Québec', '(418) 507-0000', 250);
+INSERT INTO Users VALUES (NULL, 'yuk@ulaval.ca', MD5('yuk'), 'Yuxuan', 'Zhao', 'Male', '1998-01-01', 'Québec', '(418) 507-0001', 250);
 INSERT INTO Users VALUES (NULL, 'jean@ulaval.ca', MD5('jean'), 'JC', 'Drouin', 'Male', '1998-01-01', 'Québec', '(418) 507-0421', 50);
-INSERT INTO Users VALUES (NULL, 'claude@ulaval.ca', MD5('claude'), 'claude', 'Drouin', 'Male', '1997-01-01', 'Saguenay', '(418) 523-8542', 20);
-INSERT INTO Users VALUES (NULL, 'roland@ulaval.ca', MD5('roland'), 'roland', 'charest', 'Male', '1980-10-02', 'Montréal', '(418) 507-1234', 100);
-INSERT INTO Users VALUES (NULL, 'robert@ulaval.ca', MD5('robert'), 'Robert', 'Lavoie', 'Male', '1998-01-01', 'Mauricie', '(418) 645-0530', 503);
-INSERT INTO Users VALUES (NULL, 'jacques@ulaval.ca', MD5('jacques'), 'Jacques', 'Christophe', 'Male', '1995-03-12', 'Gaspésie', '(418) 507-0000', 27);
-INSERT INTO Users VALUES (NULL, 'damien@ulaval.ca', MD5('damien'), 'Damien', 'Ferland', 'Male', '1970-01-11', 'Montréal','(418) 505-0021' , 59);
-INSERT INTO Users VALUES (NULL, 'james@ulaval.ca', MD5('james'), 'James', 'Bouchard', 'Male', '1950-10-10', 'Québec', '(418) 222-0200', 74);
-INSERT INTO Users VALUES (NULL, 'joe@ulaval.ca', MD5('joe'), 'Joe', 'Joe', 'Male', '1998-01-01', 'Québec', '(418) 507-0000', 52);
-INSERT INTO Users VALUES (NULL, 'jeanne@ulaval.ca', MD5('jeanne'), 'Jeanne', 'Lacroix', 'Female', '1979-01-10','Saguenay' , '(418) 536-3223', 52);
-INSERT INTO Users VALUES (NULL, 'catherine@ulaval.ca', MD5('catherine'), 'Catherine', 'Jean', 'Female', '1944-01-01', 'Québec','(418) 514-4234' , 52);
-INSERT INTO Users VALUES (NULL, 'roxanne@ulaval.ca', MD5('roxanne'), 'Roxanne', 'Dorval', 'Female', '1998-12-12', 'St-Tite','(418) 512-5433' , 52);
-INSERT INTO Users VALUES (NULL, 'celine@ulaval.ca', MD5('celine'), 'Celine', 'Dion', 'Female', '1970-02-12', 'Québec', '(418) 534-2534', 52);
 
+/* Cars table INSERT example */
 INSERT INTO Cars VALUES ("AAA AAA", 1, "Toyota", "yaris", 4, 2000, "black");
 INSERT INTO Cars VALUES ("BBB BBB", 2, "Mazda", "3", 4, 2005, "black");
 INSERT INTO Cars VALUES ("CCC CCC", 4, "Tesla", "X", 4, 2018, "Green");
-INSERT INTO Cars VALUES ("DDD DDD", 6, "Honda", "Civic", 4, 1999, "White");
-INSERT INTO Cars VALUES ("EEE EEE", 7, "Volvo", "p250", 4, 2009, "black");
 
-
+/* Trips table INSERT example */
 INSERT INTO Trips VALUES (NULL, "2020-05-05 08:00:00", 'Saguenay', 'Quebec', 150, 4, 2, 5, "/1/2/3", 90, "AAA AAA" );
 INSERT INTO Trips VALUES (NULL, "2020-06-20 08:00:00", 'Montréal', 'Saguenay', 300, 3, 0, 10, "/1/5/3", 120, "BBB BBB" );
 INSERT INTO Trips VALUES (NULL, "2020-07-10 08:00:00", 'Saguenay', 'Quebec', 150, 6, 1, 8, "/1/2/3/5", 30, "CCC CCC" );
-INSERT INTO Trips VALUES (NULL, "2020-05-14 08:00:00", 'St-Tite', 'Trois-Rivières', 200, 2, 2, 9, "/2/3", 150, "DDD DDD" );
-
-
-
-
+*/
 
 
 INSERT INTO Destination_pictures VALUES ('default', '../../static/images/welift.jpg');
